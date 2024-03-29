8cda3d458ed0fe08126a60777e5c7e8176cf3098
akielaries <akiel@akiel.org>
2024-03-28 23:34:48 -0700

    speeding up blink

--- blinky2.v
+++ blinky2.v
@@ -9,7 +9,7 @@ reg [23:0] counter;
 always @(posedge sys_clk or negedge sys_rst_n) begin
     if (!sys_rst_n)
         counter <= 24'd0;
-    else if (counter < 24'd1349_9999)       // 0.5s delay
+    else if (counter < 24'd674_9999)       // 0.5s delay
         counter <= counter + 1'd1;
     else
         counter <= 24'd0;
@@ -18,7 +18,7 @@ end
 always @(posedge sys_clk or negedge sys_rst_n) begin
     if (!sys_rst_n)
         led <= 6'b111110;
-    else if (counter == 24'd1349_9999)       // 0.5s delay
+    else if (counter == 24'd674_9999)       // 0.5s delay
         led[5:0] <= {led[4:0],led[5]};
     else
         led <= led;
