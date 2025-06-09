fbfce2262f29fdc244bdc8f1b3a8d01f7d089bef
akielaries <akiel@akiel.org>
2025-06-08 17:03:02 -0700

    modify send logic in test packet sender

--- uart_test.v
+++ uart_test.v
@@ -22,8 +22,8 @@ initial begin
   test_data[11] = "!";
   test_data[12] = "\r";
   test_data[13] = "\n";
-  test_data[14] = 8'hBE;
-  test_data[15] = 8'hEF;
+  test_data[14] = "\r";
+  test_data[15] = "\n";
 end
 
 reg [3:0] idx = 0;
@@ -46,26 +46,28 @@ reg [1:0] state = 0;
 always @(posedge clk) begin
   case (state)
     0: begin
-      enable_tx <= 1;
+      enable_tx <= 1;       // start TX
       state <= 1;
     end
     1: begin
-      enable_tx <= 0;
-      if (tx_done)
-        state <= 2;
+      enable_tx <= 0;       // clear after 1 cycle
+      if (tx_done) begin
+        if (idx == 15) begin
+          state <= 2;       // last byte sent, go to delay state
+        end else begin
+          idx <= idx + 1;   // send next byte
+          state <= 0;
+        end
+      end
     end
-    2: begin
-      if (delay == 24'd5_000) begin
+    2: begin                 // only after the entire test_data[] is done
+      if (delay == 24'd5_000_000) begin
         delay <= 0;
-        if (idx == 15)
-          idx <= 0;
-        else
-          idx <= idx + 1;
+        idx <= 0;           // start over
         state <= 0;
       end else
         delay <= delay + 1;
     end
   endcase
 end
-
 endmodule
